a65aa9a53161846397d7c61ca004c36ddd72d149
Juan Manuel Béc <juanmabs22@hotmail.com>
2018-01-11 21:22:54 +0100

    Ejercicio 1 hecho!

--- Tema 3 - JS/1 - Sensores/JS/global.js
+++ Tema 3 - JS/1 - Sensores/JS/global.js
@@ -1,12 +1,27 @@
+
+var circulo = document.getElementsByClassName("connection-status__circle")[0];
+var texto = document.getElementsByClassName("connection-status__text")[0];
+var last_state;
+
 function updateConectionStatus(connected) {
-    
+    if(last_state === connected) return;
+    last_state = connected;
+
+    if(connected) {
+        circulo.className = "connection-status__circle";
+        texto.innerHTML = "Conectado";
+    } else {
+        circulo.className = "connection-status__circle--disconnected";
+        texto.innerHTML = "Desconectado";
+    }
+
 }
 
 
-document.body.addEventListener("offline", function () {
+window.addEventListener("offline", function () {
     updateConectionStatus(false);
 }, false);
-document.body.addEventListener("online", function () {
+window.addEventListener("online", function () {
     updateConectionStatus(true);
 }, false);
 updateConectionStatus(navigator.onLine);
